a1d1ac0628e00c278f332ea3c0cebd3c23e4886e
Erdal dıncer <erdaldincer@Erdals-MacBook-Pro.local>
2021-03-18 11:30:36 +0000

    js and css rules has been added.

---
+++ app.js
@@ -0,0 +1,29 @@
+const navSlide=()=>{
+    const burger=document.querySelector('.burger')
+    const nav=document.querySelector('.nav-links')
+    const navLinks=document.querySelectorAll('.nav-links li')
+
+    ///toggle Nav
+    burger.addEventListener('click',()=>{
+
+        nav.classList.toggle('nav-active')
+
+
+            /// Animate Links
+    navLinks.forEach((link,index)=>{
+        if(link.style.animation){
+            link.style.animation=''
+        }else{
+            link.style.animation=`navLinkFade 0.5s ease Forwards ${index /7 +1.5}s`
+        }
+    })
+
+    //burgerAnimation
+    burger.classList.toggle('toggle')
+
+
+    })
+
+}
+
+navSlide()
